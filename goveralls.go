1982c9ec9cf16fb2ec4e0b74341a63429633757a
Yusei Yamanaka <yusei1128@gmail.com>
2022-04-27 01:01:37 +0900

    Set service to circle-ci if CIRCLECI env is set

--- goveralls.go
+++ goveralls.go
@@ -429,6 +429,8 @@ func process() error {
 
 	if *service == "" && os.Getenv("TRAVIS_JOB_ID") != "" {
 		*service = "travis-ci"
+	} else if *service == "" && os.Getenv("CIRCLECI") != "" {
+		*service = "circleci"
 	}
 
 	sourceFiles, err := getCoverage()
